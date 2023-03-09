16179479fa38e22c5568def9650753daff0f34d2
Aschii6 <120491549+Aschii6@users.noreply.github.com>
2023-03-09 15:16:52 +0200

    Update L1.c

--- OOP/L1.c
+++ OOP/L1.c
@@ -3,6 +3,7 @@
 // 3.  Determina toate reprezentarile posibile a unui numar natural ca suma
 //   de numere naturale consecutive.
 
+// primeste un natural n si afiseaza posibilitatile de sume de numere consecutive care sunt egale cu n
 void suma_de_numere(int n){
     for (int i=1; i <= n; i++){
         int sum = i;
@@ -19,16 +20,63 @@ void suma_de_numere(int n){
     }
 }
 
-void run(){
-    printf("Introduceti un numar sau 0 pentru a finaliza\n");
-    int n;
-    while (1){
-        printf("n=");
-        scanf("%d", &n);
-        if (n == 0)
-            break;
+int prim(int n){
+    for (int i=2; i<=n/2; i++)
+        if (n%i == 0)
+            return 0;
+    return 1;
+}
+
+void termeni(int n){
+    // afiseaza primi n termeni din sirul cerut
+    int i = 1;
+    int elem = 1;
+    int parti;
+    while(i<=n){
+        if (prim(elem) == 1)
+            for (int j=elem; j >= 1; j--){
+                if (i>n)
+                    break;
+                printf("%d, ", j);
+                i++;
+            }
         else{
-            suma_de_numere(n);
+            printf("%d, ", elem);
+            i++;
+            for (parti = 2; parti <= n/2; parti++)
+                if (elem % parti == 0 && prim(parti) == 1)
+                    for (int j=1; j<=parti; j++){
+                        if (i>n)
+                            break;
+                        printf("%d, ", parti);
+                        i++;
+                    }
+        }
+        elem++;
+    }
+    printf("\n");
+}
+
+void run(){
+    // meniul care permite instructiuni repetate
+    int c;
+    printf("3 ptr ex.3 sau 5 ptr ex.5\n");
+    scanf("%d", &c);
+
+    if (c ==3 || c == 5){
+        printf("Introduceti un numar sau 0 pentru a finaliza\n");
+        int n;
+        while (1){
+            printf("n=");
+            scanf("%d", &n);
+            if (n == 0)
+                break;
+            else{
+                if (c == 3)
+                    suma_de_numere(n);
+                else if (c == 5)
+                    termeni(n);
+            }
         }
     }
 }
