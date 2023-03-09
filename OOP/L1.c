79b16438babfe8a4251d59060e9ad4057e1df4ae
Aschii6 <120491549+Aschii6@users.noreply.github.com>
2023-03-09 14:06:17 +0200

    Update L1.c

--- OOP/L1.c
+++ OOP/L1.c
@@ -3,13 +3,7 @@
 // 3.  Determina toate reprezentarile posibile a unui numar natural ca suma
 //   de numere naturale consecutive.
 
-int main() {
-    int n;
-    printf("n=");
-    scanf("%d", &n);
-
-    printf("%d\n", n);
-
+void suma_de_numere(int n){
     for (int i=1; i <= n; i++){
         int sum = i;
         for (int j=i+1; j<=n; j++){
@@ -23,6 +17,23 @@ int main() {
                 break;
         }
     }
+}
 
+void run(){
+    printf("Introduceti un numar sau 0 pentru a finaliza\n");
+    int n;
+    while (1){
+        printf("n=");
+        scanf("%d", &n);
+        if (n == 0)
+            break;
+        else{
+            suma_de_numere(n);
+        }
+    }
+}
+
+int main() {
+    run();
     return 0;
 }
