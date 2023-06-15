5390a10f770a21c778859c017080fc93c2a86c09
Aschii6 <120491549+Aschii6@users.noreply.github.com>
2023-06-15 21:32:45 +0300

    Add files via upload

---
+++ SDA/L7/main.cpp
@@ -0,0 +1,14 @@
+#include "TestScurt.h"
+#include "TestExtins.h"
+#include "TestPersonal.h"
+
+
+// 5. TAD Colecție cu elemente de tip TComparabil – reprezentare prin perechi de forma (element, frecvență),
+// folosind un ABC reprezentat înlănțuit, cu înlănțuiri reprezentate pe tablou.
+
+int main() {
+    testPersonal();
+    testAll();
+    testAllExtins();
+    return 0;
+}
